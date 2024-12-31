de818a644e8979d6f9b6dba33499525e4753a20c
Leopold Perné <herrvonvoid@gmx.net>
2024-12-31 16:41:21 +0100

    feat: uninstall fish

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -343,7 +343,6 @@ in {
         packages = with pkgs; [
             croc                    # securely send files
             fd                      # find alternative
-            fish                    # like bash but more user friendly
             fzf                     # for better bash history search (ctrl-r); see bashrc
             libfaketime             # provides faketime command
             pandoc                  # markup converter
