69f7ff0d1164d157fa11f0243bb27a2ffad79677
Leopold Perné <herrvonvoid@gmx.net>
2025-03-05 20:42:18 +0100

    ref: only accept BARE repos without worktrees
    otherwise one cannot push to them, which is the whole point of having a bare repo

--- setup-utils.sh
+++ setup-utils.sh
@@ -262,8 +262,17 @@ setup_dotfiles() {
 
     # create bare clone if necessary
     if pushd "$BARE"; then
-        if isRepo "$BARE" && [ "$(git rev-parse --is-bare-repository)" = true ]; then
-            warnmsg "Reusing existing BARE ('$BARE'), thus ignoring any --remote argument"
+        if isRepo && [ "$(git rev-parse --is-bare-repository)" = true ]; then
+            # BARE must not have any worktrees, otherwise one cannot push to it, which is the whole
+            # point. sadly this is not already checked by --is-bare-repository
+            local wtlist
+            wtlist="$(git rev-parse --git-dir)"/worktrees
+            if [[ -d "$wtlist" && $(ls -1 "$wtlist" | wc -l) -gt 0 ]]; then
+                errmsg "failed to reuse folder '$BARE' as BARE: has worktrees"
+                return $OPT_ERR
+            else
+                warnmsg "Reusing existing BARE ('$BARE'), thus ignoring any --remote argument"
+            fi
         else
             errmsg "failed to reuse folder '$BARE' as BARE: not a repo or not bare"
             return $OPT_ERR
