ae96aeac3aac57dbb6dc6be1e998c074afa219ee
Leopold Perné <herrvonvoid@gmx.net>
2025-05-25 18:56:33 +0200

    fix: rebuilding always needs network
    -  always requires network
    - any non-zero exit codes in a service script make the build fail
    - this patch ensures there is no non-zero exit code in this script
    
    while there is a case in which I would want this to fail (when
    there is no flathub remote yet), this should never happen
    because the first time this config runs, nothing else would
    be installed and thus network would be required by other
    config parts anyways...

--- linux/etc/nixos/configuration.nix
+++ linux/etc/nixos/configuration.nix
@@ -299,7 +299,7 @@ in {
         wantedBy = ["multi-user.target"];
         path = [ pkgs.flatpak ];
         script = ''
-            flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+            flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo || true
         '';
     };
     # - NOTE: if flatpaks complain about not finding fonts, try:
